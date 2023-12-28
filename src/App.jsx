cbb77b7bdafd00b2ff3a5dc824b9af42bdff6b53
Menahasrinivas <menahasrinivas@gmail.com>
2023-12-28 10:13:48 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,35 @@
-import React, {useState} from "react";
-import Card from "./components/Card";
-import TopContent from "./components/TopContent";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Edit from "./components/Edit";
-import All from "./components/All";
-import Completed from "./components/Completed";
-import Not_completed from "./components/Not_completed"; 
+import React, { useState } from 'react'
+import TopBar from './Components/TopBar'
+import Cards from './Components/Cards'
 
-const App = () => {
-  let [task, setTask] = useState([
+function App() {
+  let [todo,setTodo]=useState([
     {
-      id: 1,
-      name: "Today task -1 ",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa dolorum, numquam",
-      isCompletion: true,
-    },
-    {
-      id: 2,
-      name: "Office Task -2",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur ecusandae illum? Corporis, qui?",
-      isCompletion: false,
-    },
-    {
-      id: 3,
-      name: "Today task complete pannanum",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisi usandae illum? Corporis, qui?",
-      isCompletion: true,
-    },
-  ])
+    id:1,
+    title:" Office Task-1",
+    description:"this is the description for my first task",
+    status:false
 
-  return (
-    <>
-      <div className="container-fluid mt-4 mb-5">        
-        <BrowserRouter>
-          <Routes>
-            <Route path="/home" element={<TopContent task={task} setTask={setTask} />}>
-              <Route path="all" element={<All task={task} setTask={setTask} />}></Route>
-              <Route path="completed" element={<Completed task={task} setTask={setTask} />}></Route>
-              <Route path="not-completed" element={<Not_completed task={task} setTask={setTask} />}></Route>
-            </Route>
-            <Route path="/edit/:id" element={<Edit task={task} setTask={setTask} />}/>
-            <Route path="/*" element={<Navigate to="/home/all" />}/>
-          </Routes>
-        </BrowserRouter>
-        
-      </div>
-    </>
-  );
-};
+  },
+  {
+    id:2,
+    title:" Office Task-2",
+    description:"this is the description for my second task",
+    status:false
+    
+  },
+  {
+    id:3,
+    title:" Office Task-3",
+    description:"this is the description for my third task",
+    status:false
+    
+  }
+])
+let [completed,setCompleted]=useState("All")
+  return <>
+  <TopBar todo={todo} setTodo={setTodo} completed={completed} setCompleted={setCompleted}/>
+  </>
+}
 
-export default App;
+export default App
